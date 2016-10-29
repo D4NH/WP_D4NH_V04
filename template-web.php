a8ffc71bcb8b9b8ce12e606503fdeb51caf9053f
Danh Nguyen <nguyen.dtd@gmail.com>
2016-10-29 19:49:57 +0200

    fixed titles view on web template

--- template-web.php
+++ template-web.php
@@ -41,6 +41,23 @@
             ?>
         </div>
       </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="sectiontitle">Developers.nl</h2>
+        </div>
+      </div>
+      <div class="row other">
+        <div class="col-md-4">
+            <a class="gradient" href="https://mijn.overheid.nl/" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/mijnoverheid.jpg" alt="MijnOverheid"></a>
+            <div class="subsection">
+              <div class="subtitle">MijnOverheid</div>
+              <div class="category"><i class="fa fa-cogs"></i> VueJS</div>
+              <div class="clock"><i class="fa fa-archive"></i> Web Development</div>
+              <div class="clearfix"></div>
+            </div>
+          </div>
+      </div>
       <div class="row">
         <div class="col-md-12">
           <h2 class="sectiontitle">Luminis</h2>
@@ -48,11 +65,9 @@
       </div>
       <div class="row other">
         <div class="col-md-4">
-          <a class="gradient" href="//www.monuta.nl/verzekeringsmodule/" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/monuta.jpg" alt="Monuta Verzekeringsmodule"></a>
-          <div class="subtitle">
-            <span>Monuta Verzekeringsmodule</span>
-          </div>
+          <a class="gradient" href="https://www.monuta.nl/verzekeringsmodule/" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/monuta.jpg" alt="Monuta Verzekeringsmodule"></a>
           <div class="subsection">
+            <div class="subtitle">Monuta Verzekeringsmodule</div>
             <div class="category"><i class="fa fa-cogs"></i> AngularJS</div>
             <div class="clock"><i class="fa fa-archive"></i> Web Development</div>
             <div class="clearfix"></div>
@@ -60,11 +75,9 @@
         </div>
 
         <div class="col-md-4">
-          <a class="gradient" href="//vandijk.nl/webshop" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/vandijk.jpg" alt="Van Dijk Webshop"></a>
-          <div class="subtitle">
-            <span>Van Dijk Webshop</span>
-          </div>
+          <a class="gradient" href="https://vandijk.nl/webshop" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/vandijk.jpg" alt="Van Dijk Webshop"></a>
           <div class="subsection">
+            <div class="subtitle">Van Dijk Webshop</div>
             <div class="category"><i class="fa fa-cogs"></i> AngularJS</div>
             <div class="clock"><i class="fa fa-archive"></i> Web Development</div>
             <div class="clearfix"></div>
@@ -72,34 +85,18 @@
         </div>
 
         <div class="col-md-4">
-          <a class="gradient" href="//amsterdam.luminis.eu" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/luminisamsterdam.jpg" alt="Luminis Amsterdam"></a>
-          <div class="subtitle">
-            <span>Luminis Amsterdam</span>
-          </div>
+          <a class="gradient" href="https://amsterdam.luminis.eu" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/luminisamsterdam.jpg" alt="Luminis Amsterdam"></a>
           <div class="subsection">
+            <div class="subtitle">Luminis Amsterdam</div>
             <div class="category"><i class="fa fa-cogs"></i> Wordpress</div>
             <div class="clock"><i class="fa fa-archive"></i> Web Development</div>
             <div class="clearfix"></div>
           </div>
         </div>
         <div class="col-md-4">
-          <a class="gradient" href="//devcon.luminis.eu" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/devcon.jpg" alt="DevCon Luminis"></a>
-          <div class="subtitle">
-            <span>DevCon Luminis</span>
-          </div>
+          <a class="gradient" href="https://devcon.luminis.eu" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/devcon.jpg" alt="DevCon Luminis"></a>
           <div class="subsection">
-            <div class="category"><i class="fa fa-cogs"></i> Wordpress</div>
-            <div class="clock"><i class="fa fa-archive"></i> Web Development</div>
-            <div class="clearfix"></div>
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <a class="gradient" href="//www.proudteachers.nl" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/proudteachers.jpg" alt="Proud Teachers"></a>
-          <div class="subtitle">
-            <span>Proud Teachers</span>
-          </div>
-          <div class="subsection">
+            <div class="subtitle">DevCon Luminis</div>
             <div class="category"><i class="fa fa-cogs"></i> Wordpress</div>
             <div class="clock"><i class="fa fa-archive"></i> Web Development</div>
             <div class="clearfix"></div>
@@ -107,11 +104,19 @@
         </div>
 
         <div class="col-md-4">
-          <a class="gradient" href="//training.luminis.eu" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/training.jpg" alt="Luminis Academy"></a>
-          <div class="subtitle">
-            <span>Luminis Academy</span>
-          </div>
+          <a class="gradient" href="http://www.proudteachers.nl" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/proudteachers.jpg" alt="Proud Teachers"></a>
           <div class="subsection">
+            <div class="subtitle">Proud Teachers</div>
+            <div class="category"><i class="fa fa-cogs"></i> Wordpress</div>
+            <div class="clock"><i class="fa fa-archive"></i> Web Development</div>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <a class="gradient" href="https://academy.luminis.eu/" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/training.jpg" alt="Luminis Academy"></a>
+          <div class="subsection">
+            <div class="subtitle">Luminis Academy</div>
             <div class="category"><i class="fa fa-cogs"></i> Wordpress</div>
             <div class="clock"><i class="fa fa-archive"></i> Web Development</div>
             <div class="clearfix"></div>
@@ -126,11 +131,9 @@
       </div>
       <div class="row other">
         <div class="col-md-4">
-          <a class="gradient" href="//acties.essent.nl/bespaarcoach/mediamarkt/index.html" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/bespaarcoach.jpg" alt="Bespaarcoach"></a>
-          <div class="subtitle">
-            <span>Bespaarcoach</span>
-          </div>
+          <a class="gradient" href="https://acties.essent.nl/bespaarcoach/mediamarkt/index.html" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/bespaarcoach.jpg" alt="Bespaarcoach"></a>
           <div class="subsection">
+            <div class="subtitle">Bespaarcoach</div>
             <div class="category"><i class="fa fa-cogs"></i> JQuery</div>
             <div class="clock"><i class="fa fa-archive"></i> Web Development</div>
             <div class="clearfix"></div>
@@ -138,11 +141,9 @@
         </div>
 
         <div class="col-md-4">
-          <a class="gradient" href="//bradfrostweb.com/demo/ish/?url=http%3A%2F%2Fmobiel.essent.nl#s" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/essentmobiel.jpg" alt="Mobiele versie Essent.nl"></a>
-          <div class="subtitle">
-            <span>Mobiele versie Essent.nl</span>
-          </div>
+          <a class="gradient" href="http://bradfrostweb.com/demo/ish/?url=http%3A%2F%2Fmobiel.essent.nl#s" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/essentmobiel.jpg" alt="Mobiele versie Essent.nl"></a>
           <div class="subsection">
+            <div class="subtitle">Mobiele versie Essent.nl</div>
             <div class="category"><i class="fa fa-cogs"></i> Mobile First</div>
             <div class="clock"><i class="fa fa-archive"></i> Web Development</div>
             <div class="clearfix"></div>
@@ -150,11 +151,9 @@
         </div>
 
         <div class="col-md-4">
-          <a class="gradient" href="//acties.essent.nl/bespaartips/" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/bespaartips.jpg" alt="Bespaartips van Essent"></a>
-          <div class="subtitle">
-            <span>Bespaartips van Essent</span>
-          </div>
+          <a class="gradient" href="https://acties.essent.nl/bespaartips/" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/bespaartips.jpg" alt="Bespaartips van Essent"></a>
           <div class="subsection">
+            <div class="subtitle">Bespaartips van Essent</div>
             <div class="category"><i class="fa fa-cogs"></i> JQuery</div>
             <div class="clock"><i class="fa fa-archive"></i> Web Development</div>
             <div class="clearfix"></div>
@@ -169,11 +168,9 @@
       </div>
       <div class="row other">
         <div class="col-md-4">
-          <a class="gradient" href="//www.philips.nl/e/kitchen/home.html" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/philipskitchen.jpg" alt="MyKitchen Philips"></a>
-          <div class="subtitle">
-            <span>MyKitchen Philips</span>
-          </div>
+          <a class="gradient" href="https://www.philips.nl/e/kitchen/home.html" target="_blank"><img class="afbeelding shadow img-responsive" src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/webdesign/philipskitchen.jpg" alt="MyKitchen Philips"></a>
           <div class="subsection">
+          <div class="subtitle">MyKitchen Philips</div>
             <div class="category"><i class="fa fa-cogs"></i> ATG Content Management System</div>
             <div class="clock"><i class="fa fa-archive"></i> Web Development</div>
             <div class="clearfix"></div>
